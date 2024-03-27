7a967e76e31968a74eadd780cdccfb3b39b9333c
Will <williamht123@yahoo.com>
2024-03-27 18:27:33 -0400

    init

---
+++ mysh.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
+
+int main(int argc, char* argv[]){
+
+return EXIT_SUCCESS;
+
+}
